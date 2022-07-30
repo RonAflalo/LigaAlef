43a014e814029393e3c10b8d753e4975c3e2273e
Mosh5 <Moshe577@gmail.com>
2022-07-30 16:35:46 +0300

    Data Verification On Game Create And Community Create

--- Liga Alef/liga-alef/src/Containers/communities/data/create.js
+++ Liga Alef/liga-alef/src/Containers/communities/data/create.js
@@ -7,6 +7,7 @@ import Alert from "react-bootstrap/Alert";
 import Button from "react-bootstrap/Button";
 
 function Create() {
+  const [errorMessage, setErrorMessage] = useState();
   const [comm, setComm] = useState({ name: "", type: "", maxmember: "" });
   const [show, setShow] = useState(false);
   const navigate = useNavigate();
@@ -22,39 +23,56 @@ function Create() {
   const addDocument = (event) => {
     event.preventDefault();
 
-    var ref = db.collection("Community").doc();
-    ref
-      .set({
-        Community_ID: ref.id,
-        Name: comm.name,
-        Type: comm.type,
-        MaxMember: comm.maxmember,
-        Members: [],
-        ActiveGames: [],
-        Admin: { Id: getUserId(), Name: getUserName() },
-      })
-      .then(() => {
-        ref.update({
-          Members: firebase.firestore.FieldValue.arrayUnion(getUserId()),
+    setErrorMessage('');
+    validateData()
+    if(errorMessage==='')
+    {
+      var ref = db.collection("Community").doc();
+      ref
+        .set({
+          Community_ID: ref.id,
+          Name: comm.name,
+          Type: comm.type,
+          MaxMember: comm.maxmember,
+          Members: [],
+          ActiveGames: [],
+          Admin: { Id: getUserId(), Name: getUserName() },
+        })
+        .then(() => {
+          ref.update({
+            Members: firebase.firestore.FieldValue.arrayUnion(getUserId()),
+          });
+          var creator = db.collection("Users").doc(getUserId());
+          creator.update({
+            Communities: firebase.firestore.FieldValue.arrayUnion(ref.id),
+          });
+          setShow(true);
+          setComm({ name: "", type: "", maxmember: "" });
+        })
+        .catch((err) => {
+          console.log("Error " + err.message);
         });
-        var creator = db.collection("Users").doc(getUserId());
-        creator.update({
-          Communities: firebase.firestore.FieldValue.arrayUnion(ref.id),
-        });
-        setShow(true);
-        navigate("/");
-      })
-      .catch((err) => {
-        console.log("Error " + err.message);
-      });
+      }
   };
+
+  function validateData()
+  {
+    if(comm.name.length<3)
+    { setErrorMessage('Community Name Is Too Short');}
+    else if(comm.type!='Soccer'&&comm.type!='Basketball'&&comm.type!='Volyball')
+    { setErrorMessage('Community Type Is Invalide');}
+    else if(comm.maxmember<1)
+    { setErrorMessage('Something Wrong With Member Limitation');}
+    else
+    { setErrorMessage('');}
+  }
 
   return (
     <>
       <div>
         <Alert show={show} variant="success">
           <Alert.Heading>Community Has Successfully Created!</Alert.Heading>
-          <p>Please Refresh the page</p>
+          <p>It's Time To Check-In And Play!</p>
           <hr />
           <div className="d-flex justify-content-end">
             <Button onClick={() => setShow(false)} variant="outline-success">
@@ -62,6 +80,7 @@ function Create() {
             </Button>
           </div>
         </Alert>
+        {<label>{errorMessage}</label>}
         <form onSubmit={addDocument}>
           <input
             type="text"
--- Liga Alef/liga-alef/src/Containers/games/data/createGame.jsx
+++ Liga Alef/liga-alef/src/Containers/games/data/createGame.jsx
@@ -6,8 +6,8 @@ import { getUserId } from "../../../Context/AuthContext";
 import firebase from 'firebase/compat/app';
 
 function Create(){
-  var fields;
-  const [game, setGame] = useState({day: "",month: "",year: "",time: "", location:"", minP:0, maxP:0, pitch:"", teamSize: 0});
+  const [errorMessage, setErrorMessage] = useState();
+  const [game, setGame] = useState({day: 0, month: 0, year: 0, time: "", location:"", minP:0, maxP:0, teamSize: 0, pitch:""});
   const [show, setShow] = useState(false);
   const [Community, setCommunity] = useState({id: "", name:""});
   const [res, setRes] = useState([]);
@@ -34,44 +34,55 @@ function Create(){
     event.preventDefault();
     setCommunity({ [key]: event.target.value });
     db.collection("Community").doc(event.target.value).get().then((value) => {
-    fields = value.data();
+    var fields = value.data();
     setCommunity({id:event.target.value, name:fields.Name});
     })
   };
 
   const addDoc = (event) => {
     event.preventDefault();
-    var ref = db.collection("Games").doc();
-    ref.set({
-      Gid: ref.id,
-      Location: game.location,
-      minP: game.minP,
-      maxP: game.maxP,
-      Pitch: game.pitch,
-      TeamSize: game.teamSize,
-      Players: [],
-      Waiting: [],
-      Voated: [],
-      Date: {Day: game.day, Month: game.month, Year: game.year},
-      Time: game.time,
-      Community: {Id: Community.id, Name: Community.name}
-    }).then(() => {
-      setShow(true);
-      var newGame = db.collection('Community').doc(Community.id);
-      newGame.update({ActiveGames: firebase.firestore.FieldValue.arrayUnion(ref.id)});
-      setGame({day: "",month: "",year: "",time: "", location:"", minP:"", maxP:"", pitch:"", teams:""});
-    }).catch((err) =>{
-      console.log("Error " + err.message);
-    })
+    validateData();
+    if(errorMessage==='')
+    {
+      var ref = db.collection("Games").doc();
+      ref.set({
+        Gid: ref.id,
+        Location: game.location,
+        minP: parseInt(game.minP),
+        maxP: parseInt(game.maxP),
+        TeamSize: parseInt(game.teamSize),
+        Pitch: game.pitch,
+        Players: [],
+        Waiting: [],
+        Voated: [],
+        Date: {Day: parseInt(game.day), Month: parseInt(game.month), Year: parseInt(game.year)},
+        Time: game.time,
+        Community: {Id: Community.id, Name: Community.name}
+      }).then(() => {
+        setShow(true);
+        var newGame = db.collection('Community').doc(Community.id);
+        newGame.update({ActiveGames: firebase.firestore.FieldValue.arrayUnion(ref.id)});
+        setGame({day: 0, month: 0, year: 0, time: "", location:"", minP:0, maxP:0, teamSize: 0, pitch:""});
+      }).catch((err) =>{
+        console.log("Error " + err.message);
+      })
+    }
   }
 
-
-
-  function GetCommunities(){
-  db.collection("Users").doc(getUserId()).get().then((value) => {
-     fields = value.data();
-     fields = fields.Communities;
-  });
+  function validateData()
+  {
+    if(game.location.length<3)
+    { setErrorMessage('Game Location Is Invalide');}
+    else if(game.minP < 2 || game.minP >= game.maxP || game.maxP < 3)
+    { setErrorMessage('Something Wrong With Players Limitation');}
+    else if(game.pitch!=='Grass'&&game.pitch!=='Sand'&&game.pitch!=='Hard'&&game.pitch!=='Inside')
+    { setErrorMessage('Pitch Type is Invalide - Please Select From Grass/Sand/Hard/Inside');}
+    else if(Community.id==='')
+    { setErrorMessage('Please Select Community Host');}
+    else if(game.day<1||game.day>31||game.month<1||game.month>12||game.year<2022)
+    { setErrorMessage('Date or Time Is Invalide');}
+    else
+    { setErrorMessage('');}
   }
 
   function addToRes(item){
@@ -80,15 +91,6 @@ function Create(){
           setRes(res => [...res, temp]);
       })
   }
-
-  function fetchAll(e){
-    e.preventDefault();
-    GetCommunities();
-    setRes([]);
-    for(const i in fields){
-        addToRes(fields[i]);
-        } 
-    }
 
     return(
         <>
@@ -101,13 +103,14 @@ function Create(){
             </div>
         </Alert>
         <div>
-        <h1>Form</h1>
+        <h1>Game Data</h1>
+        {<label>{errorMessage}</label>}
         <form onSubmit={addDoc}>
             <input type='text' name='location' value={game.location} onChange={handleChange} placeholder="Location" /><br />
-            <input type='text' name='pitch' value={game.pitch} onChange={handleChange} placeholder="Pitch" /><br />
+            <input type='text' name='pitch' value={game.pitch} onChange={handleChange} placeholder="Pitch - Grass/Sand/Hard/Inside" /><br />
             <input type='number' name='minP' value={game.minP} onChange={handleChange} placeholder="Min Players" /><br />
             <input type='number' name='maxP' value={game.maxP} onChange={handleChange} placeholder="Max Players" /><br />
-            <input type='number' name='teams' value={game.teamSize} onChange={handleChange} placeholder="Size Of Team" /><br />
+            <input type='number' name='teamSize' value={game.teamSize} onChange={handleChange} placeholder="Size Of Team" /><br />
             <input type='number' name='day' value={game.day} onChange={handleChange} placeholder="Day" /><br />
             <input type='number' name='month' value={game.month} onChange={handleChange} placeholder="Month" /><br />
             <input type='number' name='year' value={game.year} onChange={handleChange} placeholder="Year" /><br />
@@ -115,8 +118,8 @@ function Create(){
             <div>
                 <select value={Community.id} onChange={handleSelect("id")}>
                 <option value="" key="blank">Choose Community</option>
-                {res.map(option => (
-                  <option value={option.Community_ID} key={option.Name}>{option.Name}</option>
+                {res.map(comm => (
+                  <option value={comm.Community_ID} key={comm.Community_ID}>{comm.Name}</option>
                 ))}
               </select>
             </div>
